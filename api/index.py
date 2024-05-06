259de6daa38e139a87212e705377e0ce437610cf
VasilevDenis <124446723+VasilevDenis@users.noreply.github.com>
2024-05-06 13:29:37 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,44 +1,36 @@
 from flask import Flask, jsonify
-import json
-import time
 from faker import Faker
-from apscheduler.schedulers.background import BackgroundScheduler
+import time
 from flask_cors import CORS
 
 fake = Faker()
 app = Flask(__name__)
-CORS(app)  # Enable CORS for all origins
-
-json_objects = []
-
-def generate_new_object():
-    new_json_object = {
-        "id": fake.name(),
-        "from": fake.email(),
-        "subject": fake.sentence(),
-        "body": fake.text(),
-        "received": time.time()
-    }
-    if len(json_objects) >= 10:
-        json_objects.pop(0)  # Remove the oldest object if the list is larger than 10
-    json_objects.append(new_json_object)
-
-generate_new_object()  # Generate the initial JSON object
-
-scheduler = BackgroundScheduler()
-scheduler.add_job(generate_new_object, 'interval', seconds=1)
-scheduler.start()
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route('/')
 def home():
     return 'Hello!'
 
 @app.route('/messages/unread')
-def unread():
+def about():
+    messages = [
+        {
+            "id": fake.name(),
+            "from": fake.email(),
+            "subject": fake.sentence(),
+            "body": fake.text(),
+            "received": time.time()
+        }
+        for _ in range(5)
+    ]
+
     response = {
         "status": "ok",
         "timestamp": time.time(),
-        "messages": json_objects
+        "messages": messages
     }
 
     return jsonify(response)
+
+if __name__ == '__main__':
+    app.run()
